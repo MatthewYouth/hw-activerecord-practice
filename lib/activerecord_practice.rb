9edc412f89dba04adf17fe2939b77aa1409b4ddb
Suyang Ma <110492535+MatthewYouth@users.noreply.github.com>
2026-09-23 15:46:24 -0700

    Implement ActiveRecord practice queries and updates

--- lib/activerecord_practice.rb
+++ lib/activerecord_practice.rb
@@ -21,12 +21,54 @@ class Customer < ApplicationRecord
   #  You should NOT need to call Ruby library functions for sorting, filtering, etc.
 
   def self.any_candice
-    # YOUR CODE HERE to return all customer(s) whose first name is Candice
-    # probably something like:  Customer.where(....)
+    where(first: 'Candice')
   end
 
   def self.with_valid_email
-    # YOUR CODE HERE to return only customers with valid email addresses (containing '@')
+    where("email LIKE ?", '%@%')
   end
-  # etc. - see README.md for more details
+
+  def self.with_dot_org_email
+    where("email LIKE ?", '%.org')
+  end
+
+  def self.with_invalid_email
+    where("email IS NOT NULL AND email != ? AND email NOT LIKE ?", '', '%@%')
+  end
+
+  def self.with_blank_email
+    where(email: [nil, ''])
+  end
+
+  def self.born_before_1980
+    where('birthdate < ?', Time.zone.parse('1 January 1980'))
+  end
+
+  def self.with_valid_email_and_born_before_1980
+    where('email LIKE ? AND birthdate < ?', '%@%', Time.zone.parse('1 January 1980'))
+  end
+
+  def self.last_names_starting_with_b
+    where('last LIKE ?', 'B%').order(:birthdate)
+  end
+
+  def self.twenty_youngest
+    order(birthdate: :desc).limit(20)
+  end
+
+  def self.update_gussie_murray_birthdate
+    where(first: 'Gussie', last: 'Murray').update_all(birthdate: Time.parse('8 February 2004'))
+  end
+
+  def self.change_all_invalid_emails_to_blank
+    where("email IS NOT NULL AND email != ? AND email NOT LIKE ?", '', '%@%').update_all(email: '')
+  end
+
+  def self.delete_meggie_herman
+    where(first: 'Meggie', last: 'Herman').delete_all
+  end
+
+  def self.delete_everyone_born_before_1978
+    where('birthdate < ?', Time.zone.parse('1 January 1978')).delete_all
+  end
 end
